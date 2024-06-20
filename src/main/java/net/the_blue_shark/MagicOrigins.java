56222b67f899903cb7c4c69f16de3707e5dc4a57
the-blue-shark <naeyaertlukas5@gmail.com>
2024-06-20 11:52:22 +0200

    womp womp

---
+++ src/main/java/net/the_blue_shark/MagicOrigins.java
@@ -0,0 +1,17 @@
+package net.the_blue_shark;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicOrigins implements ModInitializer {
+	public static final String MOD_ID = "magic_origins";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello world!");
+	}
+}
---
+++ src/main/java/net/the_blue_shark/MagicOriginsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.the_blue_shark;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MagicOriginsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
